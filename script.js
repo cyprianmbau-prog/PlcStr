2e8c6037ac976fa201beafa69cbd15a4ac250b55
cyprianmbau <mbaucyprian@gmail.com>
2026-05-14 15:48:35 +0300

    design the js and html.

---
+++ script.js
@@ -0,0 +1,22 @@
+const destinations = [
+
+    {
+        location: "Mombasa",
+        landmarks: "Indian ocean",
+        notes: "the veiw was best.",
+
+    },
+
+    {
+        location: "Naivasha",
+        landmarks: "Lake Naivasha",
+        notes: "the journey was long but veiw was enjoyable",
+    },
+
+    {
+        location: "Kenya",
+        landmarks: "Highlands and thick forests",
+        notes: "quite enjoyalable and also fearful due to greate hieghts.",
+    }
+]
+function showText(index) { }
