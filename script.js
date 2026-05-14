29c59a9b222b12076f9990532745e3f36f105a16
cyprianmbau <mbaucyprian@gmail.com>
2026-05-15 00:29:41 +0300

    create functions and objects to hold details for hells gate and maasai mara.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// object for fort jesus
+// object for fort jesus details
 const site = {
 
     location: "Mombasa",
     landmark: "Indian Ocean",
-    notes: "Beautiful coastal attraction"
-
+    notes: "Beautiful coastal attraction",
+    time: "23 september 2025"
 };
 
 // function to display  stored object content
@@ -18,7 +18,56 @@ function showText() {
         "<p>Landmark: " + site.landmark + "</p>" +
 
 
-        "<p>Notes: " + site.notes + "</p>";
+        "<p>Notes: " + site.notes + "</p>" +
+
+        "<p>Time: " + site.time + "</p>";
 }
 
+// object for hells gate details
+const place = {
 
+    location: "Naivasha",
+    landmark: "Lake Naivasha",
+    notes: "enjoyable but the journey was long!",
+    time: "23 june 2025"
+};
+
+// function to display the hells gate content based on the object
+function showName() {
+
+    document.getElementById("hellsGate").innerHTML =
+
+
+        "Location:" + place.location +
+
+        "<p>Landmark: " + place.landmark + "</p>" +
+
+        "<p>Notes: " + place.notes + "</p>" +
+
+        "<p>Time: " + place.time + "</p>";
+}
+
+// object to hold maasai mara content
+const tour = {
+
+    location: "Narok",
+    landmark: "Scattered trees and long grasses",
+    notes: "enjoyalable to see the wild animals",
+    time: "23rd May 2025"
+};
+
+// function
+function getText() {
+
+    document.getElementById("Maasai").innerHTML =
+
+
+        "Location:" + tour.location +
+
+        "<p>Landmark: " + tour.landmark + "</p>" +
+
+
+        "<p>Notes: " + tour.notes + "</p>" +
+
+        "<p>time:" + tour.time + "</p>";
+}
